0ac8aa61e4f070d6a1d3f7d632d68c84ed8fa436
abi4679553 <abivengadajalam7708@gmail.com>
2025-12-11 13:45:34 +0530

    update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,63 +4,40 @@ import img2 from "../assests/image.png"
 
 const Home = () => {
 
-  const [numbers, setNumbers] = useState([1, 2, 3, 5, 4]);
-  const [newNumber, setNewNumber] = useState("");
-  const maxValue = numbers.length > 0 ? Math.max(...numbers) : "No values";
-  const addNumber = () => {
-    if (newNumber === "") return;
-    setNumbers([...numbers, parseInt(newNumber)]);
-    setNewNumber("");
-  };
-
-
   const [add, setadd] = useState(100);
   const [minus, setminus] = useState(200);
   const [mult, setmult] = useState(100 * 20);
-  let [state, setstate] = useState("abi")
-  const [color, setColor] = useState("bg-pink-500");
-  const [img , setImg] = useState(img1) ;
-
-
-
+  let [Name, setName] = useState("abi")
+  const [img, setImg] = useState(img1);
   return (
+
     <div>
-      <div style={{ padding: "20px", fontFamily: "Arial" }}>
-        <h1>Numbers: {numbers.join(", ")}</h1>
-        <h2>Maximum Value: {maxValue}</h2>
+    <div className="p-3 border-blue-400 border-4 w-[400px] shadow-blue-500 ml-[33%] mt-[20px] shadow-lg text-white ">
 
-        <input
-          type="number"
-          value={newNumber}
-          onChange={(e) => setNewNumber(e.target.value)}
-          placeholder="Enter number"
-        />
-        <button onClick={addNumber} style={{ marginLeft: "10px" }}>
-          Add Number
-        </button>
+      <h1 className="text-black  text-3xl text-center p-2 ">UseState</h1>
+      <div className="text-center  p-2">
+        <button onClick={() => setadd(add + 10)} className="boder-3  bg-pink-600 p-3 ">Increment</button>
+        <h2 className="text-blue-700 text-xl">Increment :{add}</h2>
       </div>
-      <div className="p-8">
-        <button onClick={() => setadd(add + 10)}>Increment</button>
-        <h2>Increment :{add}</h2>
-        <button onClick={() => setminus(minus - 50)}>Decrement</button>
-        <h2>Decrement : {minus}</h2>
-        <button onClick={() => setmult(mult * 10)}>Multplicaion</button>
-        <h2>mult : {mult}</h2>
-        <button onClick={() => setstate("saravanan")}>state</button>
-        <h2> state :{state}</h2>
-        <div  className={` p-5 w-fit  mt-5  text-white ${color}`}>
-        <button onClick={() => setColor("bg-blue-500")}>Changecolor</button>
-        <h2>color :{color}</h2>
-        </div>
-        <button onClick={() => setImg(img2)}>Image</button>
-        <img src={img} alt="img"  />
-
-
-
-
-
+      <div className="text-center  p-2" >
+        <button onClick={() => setminus(minus - 50)} className="boder-3  bg-pink-600 p-3">Decrement</button>
+        <h2 className="text-blue-700 text-xl">Decrement : {minus}</h2>
+      </div>
+      <div className="text-center  p-2 ">
+        <button onClick={() => setmult(mult * 10)} className="boder-3  bg-pink-600 p-3 ">Multplicaion</button>
+        <h2 className="text-blue-700 text-xl">mult : {mult}</h2>
+      </div>
+      <div className="text-center  p-2 ">
+        <button onClick={() => setName("xxxxxxxx")} className="boder-3  bg-pink-600 p-4 ">UserName</button>
+        <h2 className="text-blue-700 text-xl"> Name : {Name}</h2>
+      </div>
+      <div className=" text-center  p-2">
+        <button onClick={() => setImg(img2)} className="boder-3  bg-pink-600 p-2  ">Image</button>
+        <img src={img} alt="img" className="h-32 mt-5 ml-32" />
       </div>
     </div>
+    </div>
+
   );
 };
 
