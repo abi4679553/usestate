1f192578f26011dd7842a950b408b4560908ff88
abi4679553 <abivengadajalam7708@gmail.com>
2025-12-23 21:22:07 +0530

    update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='text-2xl text-white bg-blue-700  w-fit p-2 text-center '>About !!</div>
+    <div>
+      <p className='text-2xl text-white bg-blue-700  w-fit p-2 text-center '>About !!</p>
+      <p> Hell ! my web content !!</p>
+    </div>
   )
 }
 
