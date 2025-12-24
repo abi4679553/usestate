fffb93bf0a4884eae3a153bfd5229cbf918513f7
abi4679553 <abivengadajalam7708@gmail.com>
2025-12-24 05:49:18 +0530

    update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ const About = () => {
       <p> Hell ! my web content !!</p>
       <p>hello guys my web page is unique content for exm usages</p>
       <p className='text-2xl text-blue-700  p-2'> About Us !!</p>
+      <p>hello guys my web page is unique content for exm usages</p>
     </div>
   )
 }
