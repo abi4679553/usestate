f106d314f36d8a713e327707b96f090ff17bbda1
abi4679553 <abivengadajalam7708@gmail.com>
2025-12-25 20:09:44 +0530

    update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,11 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <p className='text-2xl text-white bg-blue-700  w-fit p-2 text-center '>About !!</p>
-      <p> Hell ! my web content !!</p>
-      <p>hello guys my web page is unique content for exm usages</p>
-      <p className='text-2xl text-blue-700  p-2'> About Us !!</p>
-      <p>hello guys my web page is unique content for exm usages</p>
+      <p className='text-2xl text-white bg-blue-700  w-fit p-2 text-center '>Content !!</p>
+      <p className='text-xl bg-pink-600 text-white w-fit p-2 m-5'>Home</p>
+      <p className='text-xl bg-pink-600 text-white w-fit p-2 m-5'>About</p>
+      <p className='text-xl bg-pink-600 text-white w-fit p-2 m-5'>Contact</p>
+     
     </div>
   )
 }
